77c7ffca5b5adfa64e7764237d70caecefd71c8e
ADOLFO BOCCHI <adolfo.bocchi@senacsp.edu.br>
2022-08-02 21:49:59 -0300

    alteracao rota

--- src/routes.js
+++ src/routes.js
@@ -9,9 +9,10 @@ router.get('/ping', (req, res) => {
 router.post('/signin', UserController.signin);
 router.post('/signup', UserController.signup);
 router.put('/user/:id', UserController.update);
-router.get('/user/:id', UserController.info);
+router.get('/user/:nick', UserController.info);
 router.put('/user/:id/timegame/:timeGame', UserController.timeGame);
 router.put('/user/:id/score/:score', UserController.score);
 router.get('/ranking/:qtd', UserController.ranking);
+router.get('/highScore', UserController.highScore);
 
 module.exports = router;
